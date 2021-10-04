a905447e9130540f02ebd39c996352425a54034d
unknown <avisekhgurung099@gmail.com>
2021-10-04 19:46:23 +0530

    airbnb--

---
+++ precache-manifest.bbed16f789512b1687787f3172c5a2c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52e349047bcbfa93d22ca8d6b78f8be4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f4bb3fb40a99223f86c",
+    "url": "/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "5c8bd4e4f54923475187",
+    "url": "/static/css/main.8c734f4a.chunk.css"
+  },
+  {
+    "revision": "7f4bb3fb40a99223f86c",
+    "url": "/static/js/2.7753eb31.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.7753eb31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c8bd4e4f54923475187",
+    "url": "/static/js/main.0ae5495b.chunk.js"
+  },
+  {
+    "revision": "9d9a4c99211d98368a0d",
+    "url": "/static/js/runtime-main.7b3982eb.js"
+  }
+]);
